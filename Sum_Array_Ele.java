aa1abb5a52c617a6f036ed8a9e7b46fc918688c6
MonikaGhadge25 <monikaghadage25@gmail.com>
2025-05-28 14:23:25 +0530

    initial

---
+++ Sum_Array_Ele.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Sum_Array_Ele
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+        System.out.println("Sum of array elements: " + sum);
+        
+    }
+}
