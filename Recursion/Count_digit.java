c9a9b67ddb46f58746caf3752b5331ac9bdd0a81
MonikaGhadge25 <monikaghadage25@gmail.com>
2025-05-29 08:50:18 +0530

    inital

--- Recursion/Count_digit.java
+++ Recursion/Count_digit.java
@@ -1,17 +1,18 @@
 //count digit in numeber using recursion
+
+import java.util.Scanner;
 class Count_digit
 {
     static int countDigits(int n) {
-        // Base case: if n is 0, return 0
         if (n == 0) {
             return 0;
         }
-        // Recursive case: divide n by 10 and add 1 for the current digit
         return 1 + countDigits(n / 10);
     }
 
     public static void main(String[] args) {
-        int number = 12345; // Example number
+        Scanner sc = new Scanner(System.in); 
+        int number = sc.nextInt(); 
         int digitCount = countDigits(number);
         System.out.println("Number of digits in " + number + " is: " + digitCount);
     }
