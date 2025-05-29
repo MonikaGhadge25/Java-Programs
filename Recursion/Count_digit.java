75edd2121c974f8557a60a4e6005765935808f56
MonikaGhadge25 <monikaghadage25@gmail.com>
2025-05-29 08:56:38 +0530

    initial

--- Recursion/Count_digit.java
+++ Recursion/Count_digit.java
@@ -10,10 +10,21 @@ class Count_digit
         return 1 + countDigits(n / 10);
     }
 
+    static int digitSum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n % 10 + digitSum(n / 10);
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
         int number = sc.nextInt(); 
         int digitCount = countDigits(number);
         System.out.println("Number of digits in " + number + " is: " + digitCount);
+
+        int sumOfDigits = digitSum(number);
+        System.out.println("Sum of digits in " + number + " is: " + sumOfDigits);
     }
 }
